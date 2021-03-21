da2ae99502310c785809db902b4fd480cf8ed645
tshanna <76193720+tshanna@users.noreply.github.com>
2021-03-21 18:47:49 -0400

    Add files via upload

--- CD8TcellProjectSteppables.py
+++ CD8TcellProjectSteppables.py
@@ -218,7 +218,7 @@ class CD8TcellProjectSteppable(SteppableBasePy):
             if cell.type == self.NAIVE and fAPC > 0:
                 
                 cell.type = self.cell_type.Preactivated
-            
+
             # uptake of IL2 and threshold for preactivated -> activated
             if cell.type == self.PREACTIVATED:
                 
@@ -241,6 +241,46 @@ class CD8TcellProjectSteppable(SteppableBasePy):
     def on_stop(self):
         # this gets called each time user stops simulation
         return
+
+
+
+class MitosisSteppable(MitosisSteppableBase):
+    def __init__(self,frequency=1):
+        MitosisSteppableBase.__init__(self,frequency)
+
+    def step(self, mcs):
+
+        cells_to_divide=[]
+        for cell in self.cell_list_by_type(self.EFFECTOR, self.ACTIVATED):
+            # Effector and activated t cells divide every ~8 hours
+            if mcs % 480 == 0:
+                cells_to_divide.append(cell)
+
+        for cell in cells_to_divide:
+
+            self.divide_cell_random_orientation(cell)
+            # Other valid options
+            # self.divide_cell_orientation_vector_based(cell,1,1,0)
+            # self.divide_cell_along_major_axis(cell)
+            # self.divide_cell_along_minor_axis(cell)
+
+    def update_attributes(self):
+        # reducing parent target volume
+        
+        # one cell inherits k = [0.7,1.0] fraction of parent cell
+        # other cell has 2 - k 
+        
+        self.parent_cell.targetVolume /= 2.0                  
+
+        self.clone_parent_2_child()            
+
+        # for more control of what gets copied from parent to child use cloneAttributes function
+        # self.clone_attributes(source_cell=self.parent_cell, target_cell=self.child_cell, no_clone_key_dict_list=[attrib1, attrib2]) 
+        
+        #if self.parent_cell.type==1:
+        #    self.child_cell.type=2
+        #else:
+        #    self.child_cell.type=1
         
 
 
