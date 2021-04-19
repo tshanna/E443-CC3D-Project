32485c1e80035d864e6df2c70faecb2aa0dac085
tshanna <76193720+tshanna@users.noreply.github.com>
2021-04-19 12:06:10 -0400

    Add files via upload

--- CD8TcellProjectSteppables.py
+++ CD8TcellProjectSteppables.py
@@ -23,36 +23,41 @@ class CD8TcellProjectSteppable(SteppableBasePy):
     def start(self):
         
         
-        self.plot_win = self.add_new_plot_window(title='Concentrations',
+        self.plot_win = self.add_new_plot_window(title='ODE Values',
                                                  x_axis_title='MonteCarlo Step (MCS)',
                                                  y_axis_title='Concentration', x_scale_type='linear', y_scale_type='linear',
                                                  grid=False,config_options={'legend': True})
         
 
-        self.plot_win2 = self.add_new_plot_window(title='Concentrations2',
+        self.plot_win2 = self.add_new_plot_window(title='Activated Cells',
                                                  x_axis_title='MonteCarlo Step (MCS)',
                                                  y_axis_title='Concentration', x_scale_type='linear', y_scale_type='linear',
                                                  grid=False,config_options={'legend': True})
                                                  
-        # initialize setting for Histogram
-        self.plot_win3 = self.add_new_plot_window(title='Cell Types', x_axis_title='Cell Types',
-                                                 y_axis_title='Number of Cells',config_options={'legend': True})
-        # _alpha is transparency 0 is transparent, 255 is opaque
-        self.plot_win3.add_plot(plot_name='N',style='Dots', color='cyan',size=4)
-        self.plot_win3.add_plot(plot_name='APC',style='Dots', color='red',size=4)
-        self.plot_win3.add_plot(plot_name='A',style='Dots', color='yellow',size=4)
-        self.plot_win3.add_plot(plot_name='E',style='Dots', color='purple',size=4)
-        self.plot_win3.add_plot(plot_name='P',style='Dots', color='green',size=4)                           
+        # self.plot_win3 = self.add_new_plot_window(title='Cell Types', x_axis_title='MCS',
+                                                 # y_axis_title='Number of Cells',config_options={'legend': True})
+                                                 
+        self.plot_win3 = self.add_new_plot_window(title='Effector Cells', x_axis_title='MCS',
+                                                 y_axis_title='Number of Cells',config_options={'legend': True})                                              
      
         self.plot_win.add_plot("IRa", style='Dots', color='green', size=4)
         self.plot_win.add_plot("Casp", style='Dots', color='cyan', size=4)
         #self.plot_win.add_plot("IL2cm", style='Dots', color='purple', size=4)
-        self.plot_win.add_plot("fAPC", style='Dots', color='purple', size=4)
-        self.plot_win.add_plot("IR", style='Dots', color='purple', size=4)
+        #self.plot_win.add_plot("fAPC", style='Dots', color='purple', size=4)
+        #self.plot_win.add_plot("IR", style='Dots', color='purple', size=4)
         #self.plot_win.add_plot("Fsa", style='Dots', color='blue', size=4)
         #self.plot_win.add_plot("Fs", style='Dots', color='red', size=4)
+        self.plot_win.add_plot("Tb", style='Dots', color='yellow', size=4)
         
         self.plot_win2.add_plot("Tb", style='Dots', color='yellow', size=4)
+        self.plot_win2.add_plot("Casp", style='Dots', color='cyan', size=4)
+        
+        # self.plot_win3.add_plot(plot_name='N',style='Dots', color='cyan',size=4)
+        # self.plot_win3.add_plot(plot_name='APC',style='Dots', color='red',size=4)
+        # self.plot_win3.add_plot(plot_name='A',style='Dots', color='yellow',size=4)
+        # self.plot_win3.add_plot(plot_name='E',style='Dots', color='purple',size=4)
+        # self.plot_win3.add_plot(plot_name='P',style='Dots', color='green',size=4)
+        self.plot_win3.add_plot("Casp", style='Dots', color='cyan', size=4)   
 
         
         """
@@ -67,8 +72,6 @@ class CD8TcellProjectSteppable(SteppableBasePy):
         E4: -> Fs ; lamF + ( muDF * Fsa ) - H * ( muAF * Tbcm * Fs ) - ( kF * Fs ) ;                                 // Nn-activated Fas
         E5: -> Fsa ; H * ( muAF * Tbcm * Fs ) - ( muDF * Fsa ) - ( kFa * Fsa ) ;                                     // Activated Fas
         E6: -> C ; lamc1 * ( 1/ (1 + (lamc2*IRa) ) ) * ( 1/ (1 + (lamc3*fAPC) ) ) + ( lamc4 * Fsa ) - ( kC * C ) ;  // Caspase
-        //E7: -> IL2cm ; 0.0 ;
-        //E8: -> fAPC ; 0.0 ;
         
         // Decay rates
         
@@ -102,7 +105,6 @@ class CD8TcellProjectSteppable(SteppableBasePy):
         
         // Initial conditions
         
-        secrete = 0
         IL2cm = 0
         fAPC = 0
         Tbcm = 0
@@ -128,7 +130,6 @@ class CD8TcellProjectSteppable(SteppableBasePy):
         i = 0
         while i < 33:
             x1 = random.sample(range(0,201),1)
-            print(x1[0])
             x = x1[0]
             y1 = random.sample(range(0,201),1)
             y = y1[0]
@@ -166,12 +167,11 @@ class CD8TcellProjectSteppable(SteppableBasePy):
                 cell.targetVolume = 25
                 cell.lambdaVolume = 10
                 cell.dict['lamR3'] = lamR3
-                cell.dict['Alive'] = 1
                 
                 self.add_antimony_to_cell(model_string=model_string,
                                   model_name='dp',
                                   cell=cell,
-                                  step_size=1.0)
+                                  step_size=1)
                 
 
     def step(self,mcs):
@@ -180,33 +180,52 @@ class CD8TcellProjectSteppable(SteppableBasePy):
             for cell in self.cell_list_by_type(self.PREACTIVATED):
                 if cell.sbml.dp['fAPC'] > 0:
                     self.cellOI = cell
-                    break
+                    break   
         
-        if self.cellOI:
-            if not mcs % 50 :
-            #for cell in self.cell_list_by_type(self.PREACTIVATED,self.ACTIVATED):
-                self.plot_win.add_data_point("IRa", mcs, self.cellOI.sbml.dp['IRa'])
-                #self.plot_win.add_data_point("IR", mcs, self.cellOI.sbml.dp['IR'])
-                
-                self.plot_win.add_data_point("Casp", mcs, self.cellOI.sbml.dp['C'])
-                self.plot_win.add_data_point("Fs", mcs, self.cellOI.sbml.dp['Fs'])
-                self.plot_win.add_data_point("Fsa", mcs, self.cellOI.sbml.dp['Fsa'])
-                #self.plot_win.add_data_point("Il2cm", mcs, self.cellOI.sbml.dp['IL2cm']*1E12)
-                self.plot_win.add_data_point("fAPC", mcs, self.cellOI.sbml.dp['fAPC'])
+        if not mcs % 10:
+            for cell in self.cell_list:
+                if self.cellOI:
+                    self.plot_win.add_data_point("IRa", mcs, self.cellOI.sbml.dp['IRa'])
+                    self.plot_win.add_data_point("Tb", mcs, self.cellOI.sbml.dp['Tb']) 
+                    #self.plot_win.add_data_point("fAPC", mcs, self.cellOI.sbml.dp['fAPC'])
+                    self.plot_win.add_data_point("Casp", mcs, self.cellOI.sbml.dp['C'])
+            # for cell in self.cell_list_by_type(self.ACTIVATED):
+                # if self.cellOI:
+                    # self.plot_win2.add_data_point("Tb", mcs, self.cellOI.sbml.dp['Tb']) 
+                    # self.plot_win2.add_data_point("Casp", mcs, self.cellOI.sbml.dp['C'])
+            # for cell in self.cell_list_by_type(self.EFFECTOR):
+                # if self.cellOI:
+                    # self.plot_win3.add_data_point("Casp", mcs, self.cellOI.sbml.dp['C'])
         
-                self.plot_win2.add_data_point("Tb", mcs, self.cellOI.sbml.dp['Tb']) 
-               
-        if not mcs % 10 :
-            self.plot_win3.add_data_point("N", mcs, len(self.cell_list_by_type(self.NAIVE)))
-            self.plot_win3.add_data_point("P", mcs, len(self.cell_list_by_type(self.PREACTIVATED)))
-            self.plot_win3.add_data_point("A", mcs, len(self.cell_list_by_type(self.ACTIVATED)))
-            self.plot_win3.add_data_point("E", mcs, len(self.cell_list_by_type(self.EFFECTOR)))
-            self.plot_win3.add_data_point("APC", mcs, len(self.cell_list_by_type(self.APC)))
-    
-        # field = self.field.CHEMICAL_FIELD_NAME
+        # if not self.cellOI:
+            # for cell in self.cell_list_by_type(self.PREACTIVATED):
+                # if cell.sbml.dp['fAPC'] > 0:
+                    # self.cellOI = cell.id
+                    # break
         
-        # for cell in self.cell_list:
-        # concentrationAtCOM = IL2[int(cell.xCOM), int(cell.yCOM), int(cell.zCOM)]
+        # if self.cellOI:
+            # if not mcs % 50 :
+                # cellOI = self.fetch_cell_by_id(self.cellOI)
+                # if cellOI is not None:
+                    # self.plot_win.add_data_point("IRa", mcs, cellOI.sbml.dp['IRa'])
+                     #self.plot_win.add_data_point("IR", mcs, cellOI.sbml.dp['IR'])
+                    
+                    # self.plot_win.add_data_point("Casp", mcs, cellOI.sbml.dp['C'])
+                    # self.plot_win.add_data_point("Fs", mcs, cellOI.sbml.dp['Fs'])
+                    # self.plot_win.add_data_point("Fsa", mcs, cellOI.sbml.dp['Fsa'])
+                    # #self.plot_win.add_data_point("Il2cm", mcs, cellOI.sbml.dp['IL2cm']*1E12)
+                    # self.plot_win.add_data_point("fAPC", mcs, cellOI.sbml.dp['fAPC'])
+            
+                    # self.plot_win2.add_data_point("Tb", mcs, cellOI.sbml.dp['Tb']) 
+                # else:
+                    # self.cellOI = None
+        
+        # if not mcs % 10 :
+            # self.plot_win3.add_data_point("N", mcs, len(self.cell_list_by_type(self.NAIVE)))
+            # self.plot_win3.add_data_point("P", mcs, len(self.cell_list_by_type(self.PREACTIVATED)))
+            # self.plot_win3.add_data_point("A", mcs, len(self.cell_list_by_type(self.ACTIVATED)))
+            # self.plot_win3.add_data_point("E", mcs, len(self.cell_list_by_type(self.EFFECTOR)))
+            # self.plot_win3.add_data_point("APC", mcs, len(self.cell_list_by_type(self.APC)))
     
         IL2_secretor = self.get_field_secretor('IL2')
         
@@ -216,7 +235,7 @@ class CD8TcellProjectSteppable(SteppableBasePy):
         lamR4 = 0.0
         lam1 = 1E-12
         lamT4 = 0.0
-
+        
         for cell in self.cell_list_by_type(self.APC):
             
             # movement for APC updated every 90 minutes (mcs)
@@ -244,115 +263,97 @@ class CD8TcellProjectSteppable(SteppableBasePy):
             # APC death after lifespan is reached
             if mcs >= cell.dict['lifespan']:
                 
-                cell.targetVolume = 0
+                cell.targetVolume = 0.0
         
         for cell in self.cell_list_by_type(self.NAIVE, self.EFFECTOR, self.PREACTIVATED, self.ACTIVATED):
             
-            # movement for T cells updated every 90 minutes (mcs)
-            if mcs % 90 == 0:
-                n = random.uniform(-pi,pi)
+            if cell.targetVolume > 0.0:
                 
-                # randomize if movement vector is positive or negative
-                sign = random.uniform(-3,3)
-                abs_sign = abs(sign)
-                
-                # ensure there is no divide by 0
-                if sign == 0:
-                    sign = sign + 1
+                # movement for T cells updated every 90 minutes (mcs)
+                if mcs % 90 == 0:
+                    n = random.uniform(-pi,pi)
+                    
+                    # randomize if movement vector is positive or negative
+                    sign = random.uniform(-3,3)
                     abs_sign = abs(sign)
+                    
+                    # ensure there is no divide by 0
+                    if sign == 0:
+                        sign = sign + 1
+                        abs_sign = abs(sign)
+                    
+                    x_temp = math.cos(n)
+                    y_temp = math.sin(n)
+                    
+                    x = 150.0*(x_temp*x_temp)*(sign/abs_sign)
+                    y = 150.0*(y_temp*y_temp)*(sign/abs_sign)
+                    
+                    cell.lambdaVecX = x
+                    cell.lambdaVecY = y            
                 
-                x_temp = math.cos(n)
-                y_temp = math.sin(n)
-                
-                x = 150.0*(x_temp*x_temp)*(sign/abs_sign)
-                y = 150.0*(y_temp*y_temp)*(sign/abs_sign)
-                
-                cell.lambdaVecX = x
-                cell.lambdaVecY = y            
-            
-            # count amount of APC a T cell is in contact with
-            fAPC = 0
-            Tbcm = 0
-            for neighbor, common_surface_area in self.get_cell_neighbor_data_list(cell):
-                
-                if neighbor:
-                    if neighbor.type == self.APC:
-                        fAPC += 1
-                    Tbcm += cell.sbml.dp['Tbcm']
-            
-            # keep track of effector-effector or effector-activated contacts (heaviside function H)
-            if cell.type == self.EFFECTOR:
+                # count amount of APC a T cell is in contact with
+                fAPC = 0
+                Tbcm = 0
                 for neighbor, common_surface_area in self.get_cell_neighbor_data_list(cell):
                     if neighbor:
-                        if neighbor.type == self.EFFECTOR or self.ACTIVATED:
-                            cell.sbml.dp['H'] = 1
-                        else:
-                            cell.sbml.dp['H'] = 0
-            
-            il2_cm = (IL2_secretor.amountSeenByCell(cell)/(cell.volume)
-            
-            #print("IL2CM VALUE IS:", il2_cm)
-            
-            #update ode
-            cell.sbml.dp['IL2cm'] = il2_cm 
-            cell.sbml.dp['fAPC'] = fAPC
-            cell.sbml.dp['Tbcm'] = Tbcm
-            
-            if cell.type == self.ACTIVATED:
-                cell.sbml.dp['IL2cm'] = 0.0
-            
-            # second term PDE
-            secrete = ( cell.dict['lamR3']*(( cell.sbml.dp['IRa'] )/( lamR4 + cell.sbml.dp['IRa'] + small_num)) + lam1 * fAPC ) * ( 1 / (1 + lamT4 * cell.sbml.dp['Tb']) )
-            cell.sbml.dp['secrete'] = secrete/(cell.surface)
-            
-            # step the simulation
-            self.timestep_sbml()
-            
-            if cell.type is not self.cell_type.Naive:
-                # secretion of IL2 by T cells 
-                IL2_secretor.secreteOutsideCellAtBoundary(cell, cell.sbml.dp['secrete'])
+                        if neighbor.type == self.APC:
+                            fAPC += 1
+                        Tbcm += cell.sbml.dp['Tbcm']
                 
-                #Caspase threshold for effector, activated, preactivated
-                if cell.sbml.dp['C'] > 2.63:
-                   cell.dict['Alive'] = False
-            
-            if cell.dict['Alive'] == False:
-                cell.type = self.DEAD
-                break
-            # step the simulation
-            # self.timestep_sbml()
-            
-            # if Tbet threshold reached, A -> E
-            if cell.type == self.ACTIVATED:
-                if cell.sbml.dp['Tb'] > 40*cell.dict['activeTB']:
-                    cell.type = self.EFFECTOR
-            
-            
-            # Preactivated cells stop moving until activated
-            if cell.type == self.PREACTIVATED:
+                # track effector-effector or effector-activated contacts (heaviside function H)
+                if cell.type == self.EFFECTOR:
+                    for neighbor, common_surface_area in self.get_cell_neighbor_data_list(cell):
+                        if neighbor:
+                            if neighbor.type == self.EFFECTOR or self.ACTIVATED:
+                                cell.sbml.dp['H'] = 1
+                            else:
+                                cell.sbml.dp['H'] = 0
                 
-                #cell.dict['activeIRa'] = 
+                il2_cm = IL2_secretor.amountSeenByCell(cell)
 
-                #if not self.cellOI:
-                 #  self.cellOI = cell
+                #update ode
+                cell.sbml.dp['IL2cm'] = il2_cm
+                cell.sbml.dp['fAPC'] = fAPC
+                cell.sbml.dp['Tbcm'] = Tbcm    
                 
-                cell.lambdaVecX = 0.0
-                cell.lambdaVecY = 0.0
+                # second term PDE
+                #secrete = ( cell.dict['lamR3']*(( cell.sbml.dp['IRa'] )/( lamR4 + cell.sbml.dp['IRa'] + small_num)) + lam1 * cell.sbml.dp['fAPC'] ) * ( 1 / (1 + lamT4 * cell.sbml.dp['Tb']) )/(cell.surface)
+                secrete = lam1*cell.sbml.dp['fAPC']
                 
-                #if IL2 threshold reached, PA -> A
-                if cell.sbml.dp['IRa'] > 2.6: #7*Mvox
-                    cell.type = self.ACTIVATED
-                    cell.dict['activeTB'] = cell.sbml.dp['Tb']
-                    #if not self.cellOI:
-                     #   self.cellOI = cell
+                # step the simulation
+                # self.timestep_sbml()
+                
+                if cell.type is not self.NAIVE:
+                    # secretion of IL2 by T cells 
+                    IL2_secretor.secreteOutsideCellAtBoundary(cell, secrete)
                     
-                    cell.dict['lamR3'] = 0.0
-            
-            # Naive -> PA
-            if cell.type == self.NAIVE and fAPC > 0:
+                    #Caspase threshold for effector, activated, preactivated
+                    if cell.sbml.dp['C'] > 2.63:
+                       cell.targetVolume = 0.0
+
+                # if Tbet threshold reached, A -> E
+                if cell.type == self.ACTIVATED:
+                    if cell.sbml.dp['Tb'] > 40:
+                        # print("Tbet VALUE AT 3000 MCS: ", cell.sbml.dp['Tb'])
+                        cell.type = self.EFFECTOR
                 
-                cell.type = self.PREACTIVATED
-                print("IRa value at preactivation: ", cell.sbml.dp['IRa'])
+                # Preactivated cells stop moving until activated
+                if cell.type == self.PREACTIVATED:
+
+                    cell.lambdaVecX = 0.0
+                    cell.lambdaVecY = 0.0
+                    
+                    #if IL2 threshold reached, PA -> A
+                    if cell.sbml.dp['IRa'] > 7: 
+                        cell.type = self.ACTIVATED                 
+                        cell.dict['lamR3'] = 0.0
+                
+                # Naive -> PA
+                if cell.type == self.NAIVE and fAPC > 0:
+                    cell.type = self.PREACTIVATED
+        
+        # step the simulation
+        self.timestep_sbml()            
 
     def finish(self):
         
